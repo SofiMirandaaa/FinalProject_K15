95dbd1afca74bb4013bb80a051780d3d33d85c89
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2023-07-05 11:47:04 +0700

    membuat tampilan form data supplier

--- FinalProject_K15/Form5.cs
+++ FinalProject_K15/Form5.cs
@@ -16,5 +16,45 @@ namespace FinalProject_K15
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
